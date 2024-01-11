ee869fba5e2effb607fb3984438bee7cf25d0ecd
enavagoro <52795648+enavagoro@users.noreply.github.com>
2024-01-11 06:28:46 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   host: 'https://medicapi-aq6tp.ondigitalocean.app',
-  qrApi: 'http://localhost:8080'
+  qrApi: 'http://159.223.150.42:8080'
 };
 
 /*

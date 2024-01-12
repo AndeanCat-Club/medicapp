8053888d07b113712e173626e6ee7afa3d3e9a2f
eliasNav <elias.navarrete@powerbe.cl>
2024-01-12 11:44:18 -0300

    feat: public medical-record

--- src/app/_services/utils.service.ts
+++ src/app/_services/utils.service.ts
@@ -9,14 +9,14 @@ export class UtilService {
     constructor() {
     }
 
-    calculateDate(currentDate: Date): string {
+    calculateDate(currentDate: Date | undefined): string {
         const currentLocale = navigator.language || 'en-US';
-        const date = new Date(currentDate,)
+        const date = new Date(currentDate || new Date())
         return date.toLocaleString(currentLocale).split(',')[0]
       }
     
-      calculateAge(dateOfBirth: Date): number {
-        const currentDateOfBirth = new Date(dateOfBirth)
+      calculateAge(dateOfBirth: Date | undefined): number {
+        const currentDateOfBirth = new Date(dateOfBirth || new Date())
         const currentDate = new Date()
         const ageInMilliseconds = currentDate.getTime() - currentDateOfBirth.getTime()
         const ageInYears = ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000)

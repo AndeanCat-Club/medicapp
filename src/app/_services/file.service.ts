f5d5241017238527b7fe4365ea0c983d16d090cd
eliasNav <elias.navarrete@powerbe.cl>
2024-01-16 23:46:03 -0300

    feat: fix buttons

--- src/app/_services/file.service.ts
+++ src/app/_services/file.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 
 @Injectable()
@@ -10,21 +12,21 @@ export class FileService {
   public fileUploaded: any;
 
   constructor(private http: HttpClient) { }
- 
-  sendFile(formData: FormData){
-    formData.append('privacy', 'private');   
+
+  sendFile(formData: FormData) {
+    formData.append('privacy', 'private');
     formData.append('storageClient', environment.storageClient)
     return this.http.post<any>(`${this.url}/file/createFile`, formData, {});
   }
 
-  readFile(filePath: String){
+  readFile(filePath: String) {
     const body = {
       storageClient: environment.storageClient,
-      storageData:{
-          "filePath":filePath
+      storageData: {
+        "filePath": filePath
       }
     }
-    
+
     const options = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
       responseType: 'blob' as 'json'
@@ -33,11 +35,18 @@ export class FileService {
     return this.http.post<any>(`${this.url}/file/getFile`, body, options);
   }
 
-  createFile(formData: FormData){
+  createFile(formData: FormData) {
     formData.append('privacy', 'private');
     formData.append('storageClient', environment.storageClient)
     return this.http.post<any>(`${this.url}/file/createFile`, formData, {});
   }
 
-  
+  urlToBlob(imageUrl: string): Observable<string> {
+    return this.http.get(imageUrl, { responseType: 'arraybuffer' }).pipe(
+      map((data: ArrayBuffer) => {
+        const blob = new Blob([data], { type: 'image/jpeg' });
+        return URL.createObjectURL(blob);
+      })
+    );
+  }
 }
